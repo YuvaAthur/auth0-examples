3c3cde4340e7fe3d90dba42ff5064bcef4d006c8
George A. Mihaila <george.mihaila@gmail.com>
2019-03-13 17:08:48 -0700

    initial

---
+++ client/auth0-variables.js
@@ -0,0 +1,5 @@
+var AUTH0_CLIENT_ID = 'eJnuipnMuf2NSXcsiwSfAPn8j-CgPBIQ';
+var AUTH0_DOMAIN = 'mihaila.auth0.com';
+var AUTH0_AUDIENCE = 'http://0.0.0.0:3010/api/private';
+var AUTH0_CALLBACK_URL = location.href;
+
